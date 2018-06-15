c5c8ba5b429de52e72a0f4e191fb81f85156c9bd
佐藤　大輝 <c011425081@edu.teu.ac.jp>
2018-06-15 23:50:56 +0900

    taiki

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
-
+    int a = 0;
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
